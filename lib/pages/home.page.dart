a9a9001e7ac14803e675e8eb8fd60da23195b2dc
Viktor Kindrat <84918090+viktor-kindrat@users.noreply.github.com>
2026-02-22 12:53:45 +0200

    Fix dart issues

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -22,17 +22,17 @@ class _MyHomePageState extends State<MyHomePage> {
         icon: Icon(
           _selectedTab == 0 ? Icons.leaderboard : Icons.leaderboard_outlined,
         ),
-        label: "Leaders",
+        label: 'Leaders',
       ),
       BottomNavigationBarItem(
         icon: Icon(_selectedTab == 1 ? Icons.gamepad : Icons.gamepad_outlined),
-        label: "Play",
+        label: 'Play',
       ),
       BottomNavigationBarItem(
         icon: Icon(
           _selectedTab == 2 ? Icons.settings : Icons.settings_outlined,
         ),
-        label: "Settings",
+        label: 'Settings',
       ),
     ];
   }
@@ -43,13 +43,13 @@ class _MyHomePageState extends State<MyHomePage> {
       body: const Center(
         child: Column(
           mainAxisAlignment: .center,
-          children: [Text("hello world")],
+          children: [Text('hello world')],
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedTab,
         onTap: _onMenuTap,
-        selectedIconTheme: IconThemeData(size: 32),
+        selectedIconTheme: const IconThemeData(size: 32),
         items: _bottomMenu,
       ),
     );
